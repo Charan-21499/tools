4d45da9499094db4578154fa814a958a47f8e5e5
RangerOne-1 <56217763+RangerOne-1@users.noreply.github.com>
2020-07-07 21:24:15 +0530

    Update js_beautifier.py
    added comments

--- scripts/js_beautifier.py
+++ scripts/js_beautifier.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python
-
+#usage js_beautifier.py file
 import sys
 import jsbeautifier
 import os
 from bs4 import BeautifulSoup
 
+#used to take file name from command line
 name = str(sys.argv[1])
+
+#js beautifying
 res = jsbeautifier.beautify_file(name).encode('utf-8')
+
+#removing exsisting file
 rm = 'rm -f '+name
 os.system(rm)
 
+# html beautifying
 soup = BeautifulSoup(res,'html.parser')
 ress = (soup.prettify()).encode('utf-8')
 
-
+#creating a file with a predefined name and writting to it 
 f=open(name,'w')
 f.write(ress)
 f.close()
